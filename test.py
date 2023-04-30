f8ea648f3f6c89cc9ac1d822cb99120d9f12c706
AimanZaharin <noraimanzaharinnoorazwan11@gmail.com>
2023-04-30 19:21:03 +0800

    Trial and Error using Tkinter

--- test.py
+++ test.py
@@ -1,22 +1,25 @@
+import sys
+from tkinter import *
+
+def assign(value):
+    global x
+    x = value
+    mGui.destroy()
+
+mGui = Tk()
+mGui.geometry("500x100+500+300")
+mGui.title("Attribute Selection Window")
+
+frame1 = Frame(mGui)
+frame1.pack()
+
+mLabel = Label(frame1, text = "Please select one of the following attributes to assign to the selected Convwks feature:").grid(row=0, column=0)
+
+frame2 = Frame(mGui)
+frame2.pack()
 
 
-def main():
-    Coins = [6, 5, 1];
-    value = 9
-
-    numOfways = [0] * (value +1)
-    numOfways[0] = 1
-
-    for i in range(len(Coins)):
-        for j in range(len(numOfways)):
-            if (Coins[i] <= j):
- 
-                # Update the ways array
-                numOfways[j] += numOfways[(int)(j - Coins[i])];
-
-    print(numOfways[value])
-
-
-    
-
-main()
+mButton = Button(frame2, text = "CON", command = lambda: assign("CON")).grid(row=0, column=0, padx=10)
+mButton = Button(frame2, text = "MS", command = lambda: assign("MS")).grid(row=0, column=1, padx=10)
+mButton = Button(frame2, text = "DRN", command = lambda: assign("DRN")).grid(row=0, column=2, padx=10)
+mGui.mainloop()     #FOR WINDOWS ONLY

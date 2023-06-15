f312f5f1794fb8975a1524a9939519fcb76b2995
AimanZaharin <noraimanzaharinnoorazwan11@gmail.com>
2023-06-15 12:38:29 +0800

    ni

--- DSAII/DSAII - Assignment25_2115931.py
+++ DSAII/DSAII - Assignment25_2115931.py
@@ -1,15 +1,15 @@
 import sys
 
 class Graph():
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = [[0 for column in range(vertices)]
-                      for row in range(vertices)]
+    def __init__(self, vert):
+        self.V = vert
+        self.graph = [[0 for column in range(vert)]
+                      for row in range(vert)]
     
-    def printMST(self, parent):
+    def printMST(self, par):
         print("Edge \tWeight")
         for i in range(1, self.V):
-            print(parent[i], "-", i, "\t", self.graph[i][parent[i]])
+            print(par[i], "-", i, "\t", self.graph[i][par[i]])
     
     def minKey(self, key, mstSet):
  
